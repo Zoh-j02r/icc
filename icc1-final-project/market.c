ea64aa134e24e28837d8213593d8a2468bd816ce
Zoh-j02r <saulobruno@usp.br>
2023-07-15 16:01:54 -0300

    feat: added dynamic product allocation

--- icc1-final-project/market.c
+++ icc1-final-project/market.c
@@ -1,19 +1,18 @@
 #include "market.h"
 #include "string.h"
 
-void end_task()
+void end_task(void)
 {
-	printf("\n--------------------------------------------------\n");
+	printf("--------------------------------------------------\n");
 }
 
 void parse_stock(market* shop)
 {
-	// SOLVE: "Seu Zé" thinks that doing business with you was the greatest mistake 
-	// of his entire life (He is 82 years old by the way)
 	FILE *data = fopen("data.bin", "rb");
-	fread(&shop->max_stock_space,sizeof(int),1,data);
-        create_empty_stock(shop,shop->max_stock_space);
-	for (int i = 0; i < shop->max_stock_space; i++)
+	fread(&shop->stock_space,sizeof(int),1,data);
+	fread(&shop->balance,sizeof(double),1,data);
+        create_empty_stock(shop,shop->stock_space);
+	for (int i = 0; i < shop->stock_space; i++)
 	{
 		fread(&shop->stock[i],sizeof(product),1,data);
  	}
@@ -25,16 +24,14 @@ void purge_stock(market* shop)
 	FILE *data = fopen("data.bin", "wb");
     	if (data == NULL)
 	{
-		// SOLVE: The "Seu Zé" is losting money every time this application
-		// crashes, do you really want to end up with his business?
         	printf("Failed to open the file.\n");
         	exit(EXIT_FAILURE);
     	}
 
-	fwrite(&shop->max_stock_space,sizeof(int),1,data);
-	for (int i = 0; i < shop->max_stock_space; i++)
+	fwrite(&shop->stock_space,sizeof(int),1,data);
+	fwrite(&shop->balance,sizeof(double),1,data);
+	for (int i = 0; i < shop->stock_space; i++)
 	{
-		// SOLVE: "Seu Zé" after a long day of work lost the entire stock data 
 		fwrite(&shop->stock[i],sizeof(product),1,data);
  	}
 	free(shop->stock);
@@ -43,12 +40,18 @@ void purge_stock(market* shop)
 
 void create_empty_stock(market* shop,int initial_stock)
 {
-	shop->stock = malloc(initial_stock * sizeof(product));
-	for (int i = 0; i < initial_stock; i++)
+	if (initial_stock == 0)
 	{
-		clear_item(&shop->stock[i]);
- 	}
-	shop->balance = 100.0f;
+		// what?
+	}
+	else
+	{
+		shop->stock = malloc(initial_stock * sizeof(product));
+		for (int i = 0; i < initial_stock; i++)
+		{
+			clear_item(&shop->stock[i]);
+ 		}
+	}
 }
 
 void clear_item(product* item)
@@ -60,14 +63,15 @@ void clear_item(product* item)
 
 void check_stock(market* shop)
 {
-	// Must check if a stock file already exist, if not create a empty new stock
 	FILE *data = fopen("data.bin", "rb");
 	if (data == NULL)
 	{
 		// If openning the file fails it assumes the file do not exist
 		int stock_size;
-		scanf("%d",&stock_size);
-		shop->max_stock_space = stock_size;
+		double stock_balance;
+		scanf("%d %lf",&stock_size,&stock_balance);
+		shop->stock_space = stock_size;
+		shop->balance = stock_balance;
         	create_empty_stock(shop,stock_size);
 	}
 	else
@@ -86,92 +90,114 @@ void select_operation(market* shop,int op)
 	switch (op)
 	{
 		case 153:
-			// SOLVE: is there a way to make this better looking?
-			scanf(" %s",name);
-			scanf(" %d",&amount);
-			scanf(" %lf",&price);
+			scanf(" %s %d %lf",name,&amount,&price);
+			//printf(" %s %d %lf\n",name,amount,price);
 			insert_stock_item(shop,name,amount,price);
 			break;
 		case 134:
-			scanf(" %d",&amount);
-			scanf(" %d",&tag);
-			increase_product_amount(&shop->stock[tag],amount);
+			scanf(" %d %d",&tag,&amount);
+			increase_product_amount(&shop->stock[tag].amount,amount);
 			break;
 		case 157:
-			// SOLVE: get rid of this aux variable
-			scanf(" %lf",&price);
-			scanf(" %d",&tag);
-			modify_item_price(&shop->stock[tag],price);
+			scanf(" %d %lf",&tag,&price);
+			//printf(" %d %lf\n",tag,price);
+			modify_item_price(&shop->stock[tag].price,price);
 			break;
 		case 155:
 			sell_item(shop);
+			end_task();
 			break;
 		case 136:
 			list_stock(shop);
+			end_task();
 			break;
 		case 150:
 			check_balance(shop->balance);
+			end_task();
 			break;
 		case 139:
 			purge_stock(shop);
 			break;
  	}
-	if (op != 153)
-	{
-		end_task();
-	}
 }
 
 void insert_stock_item(market* shop,char* name,int amount,double price)
 {
-	for (int i = 0; i < shop->max_stock_space; i++)
+	int count = 0;
+	int avail = 0;
+	for (int i = 0; i < shop->stock_space; i++)
 	{
-		if (shop->stock[i].amount == 0)
+		if (shop->stock[i].price == 0.0f && shop->stock[i].amount == 0)
 		{
-			
-			strcpy(shop->stock[i].name,name);
-			shop->stock[i].amount = amount;
-			shop->stock[i].price = price;
+			count++;
+		}
+	}
+	for (int i = 0; i < shop->stock_space; i++)
+	{
+		if (shop->stock[i].price == 0.0f && shop->stock[i].amount == 0)
+		{
+			avail = i;
 			break;
 		}
- 	}
+	}
+	if (count == 0)
+	{
+		avail = shop->stock_space;
+		change_stock_size(shop,avail+1);
+		strcpy(shop->stock[avail].name,name);
+		shop->stock[avail].amount = amount;
+		shop->stock[avail].price = price;
+	}
+	else
+	{
+		strcpy(shop->stock[avail].name,name);
+		shop->stock[avail].amount = amount;
+		shop->stock[avail].price = price;
+	}
 }
 
 void change_stock_size(market* shop,int size)
 {
-	shop->stock = realloc(shop->stock, size);
+	if(size == 0)
+	{
+		shop->stock = malloc(sizeof(product));
+	}
+	else
+	{
+		shop->stock = realloc(shop->stock, size);
+	}
 	if (shop->stock == NULL)
 	{
 	        printf("Unable to increase stock\n");
-	        free(shop);
+		free(shop->stock);
 	        exit(EXIT_FAILURE);
 	}
-	shop->max_stock_space = size;
+	shop->stock_space = size;
 }
-void increase_product_amount(product* stock, int amount)
+void increase_product_amount(int* stock, int amount)
 {
-	stock->amount = amount;
+	*stock += amount;
 }
 
-void modify_item_price(product* stock, double price)
+void modify_item_price(double* stock, double price)
 {
-	stock->price = price;
+	*stock = price;
 }
 
 void sell_item(market* shop)
 {
 	int val;
-	double balance;
+	double balance = 0;
 	while (1)
 	{
         	scanf(" %d", &val);
-		if (val <= shop->max_stock_space)
+		if (val <= shop->stock_space)
 		{
         		if (val == -1)
 			{
             			break;
         		}
-			if (shop->stock[val].amount)
+			if (shop->stock[val].amount != 0)
 			{
 				shop->stock[val].amount--;
 				balance += shop->stock[val].price;
@@ -185,18 +211,19 @@ void sell_item(market* shop)
 
 void list_stock(market* shop)
 {
-	for (int i = 0;  i < shop->max_stock_space; i++)
+	for (int i = 0;  i < shop->stock_space; i++)
 	{
+		//printf("%d %s %d %lf\n",i,shop->stock[i].name,shop->stock[i].amount,shop->stock[i].price);
 		printf("%d %s %d\n",i,shop->stock[i].name,shop->stock[i].amount);
  	}
 }
 
 void check_balance(double balance)
 {
-	printf("Saldo: %.2lf",balance);
+	printf("Saldo: %.2lf\n",balance);
 }
 
-int main()
+int main(void)
 {
 	market shop;
 	check_stock(&shop);
--- icc1-final-project/market.h
+++ icc1-final-project/market.h
@@ -10,13 +10,13 @@ typedef struct {
 
 typedef struct {
 	product* stock;
-	int max_stock_space;
+	int stock_space;
 	double balance;
 } market;
 
 int space_available(market* shop);
 
-void end_task();
+void end_task(void);
 void parse_stock(market* shop);
 void purge_stock(market* shop);
 void create_empty_stock(market* shop,int initial_stock);
@@ -24,8 +24,8 @@ void check_stock(market* shop);
 void select_operation(market*, int op);
 void insert_stock_item(market* shop,char* name,int amount,double price);
 void change_stock_size(market* shop,int size);
-void increase_product_amount(product* shop,int amount);
-void modify_item_price(product* stock,double price);
+void increase_product_amount(int* stock_amount,int new_amount);
+void modify_item_price(double* stock_price,double new_price);
 void clear_item(product* item);
 void check_balance(double balance);
 void sell_item(market* shop);
